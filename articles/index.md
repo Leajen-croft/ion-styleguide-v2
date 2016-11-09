77b7c014e08c7c699cc4c6e858a60a57015b8724
Liam Wright <liam.wright@ems-internet.co.uk>
2016-11-09 09:41:50 +0000

    intial commit of version 2 of ion styleguide

---
+++ articles/index.md
@@ -0,0 +1,17 @@
+# Heading
+
+[[toc]]
+
+## Sub heading 1
+Some nice text
+
+::: warning
+## Sub heading 2
+Some even nicer text `mfkl`
+
+``` lang-css
+kfkdklgjfjklgh
+fkdl;gmrklgmt
+mjfkldmjgr
+```
+:::
---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var tap = require('gulp-tap');
+var MarkdownIt = require('markdown-it');
+var md = new MarkdownIt();
+
+md.use(require("markdown-it-anchor"), 'level: 2'); // Optional, but makes sense as you really want to link to something
+md.use(require("markdown-it-table-of-contents"));
+md.use(require("markdown-it-container"),'warning');
+
+
+gulp.task('build', function() {
+    return gulp.src('articles/**/*.md')
+        .pipe(tap(markdownToHtml))
+        .pipe(gulp.dest('./dist'));
+});
+
+function markdownToHtml(file) {
+    var result = md.render(file.contents.toString());
+    file.contents = new Buffer(result);
+    file.path = gutil.replaceExtension(file.path, '.html');
+    return;
+}
